4c0ea7705b57982a8393086111b3ad92377aed84
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-11-07 00:56:18 -0800

    removed comments from bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const auth = require('./auth.json');
-const fetch = require('node-fetch');
 const prefix = '_';
 const scraper = require('./twitterScrape.js');
 
@@ -25,47 +24,3 @@ bot.on('message', async message => {
 });
 
 bot.login(auth.token);
-
-
-    // args = args.splice(1);
-
-    // const twitterAPICall = () => {
-    // axios.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=PUBG_help&count=3')
-    //   .then(response => response.json())
-    //   .then(data = console.log(data))
-    // };
-
-    //   if (args === 'down'){
-    //     let tweet = twitterAPICall();
-    //     message.channelID.sendMessage(tweet)
-    //   }
-
-
-// bot.on('message', async (user,userID,channelID,message,e) => {
-  
-  // if (message.substring(0,1) == prefix) {
-  //   let args = message.substring(1).split(/ +/);
-  //   let cmd = args[0].toLowerCase();
-    
-  //   args = args.splice(1);
-  //   switch(cmd) {
-      
-  //     //?ping
-  //     case 'ping':
-  //     bot.sendMessage({
-  //       to: channelID,
-  //       message: 'Pong!',
-  //     });
-  //     break;
-  //     //?down
-  //     case 'down':
-  //     const { response } = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  //     const { data } =  console.log(response.json())
-  //     bot.sendMessage({
-  //       to: channelID,
-  //       message: data
-  //     });
-  //     break;
-  //   }
-  // }
-// });
