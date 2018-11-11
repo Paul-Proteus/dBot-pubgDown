f311c29bf93cfd0031d424240d86798c68167711
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-11-11 14:38:12 -0800

    removed auth.json && made twitter scrape promise based

--- twitterScrape.js
+++ twitterScrape.js
@@ -1,39 +1,40 @@
 const cheerio = require('cheerio');
-const request = require('request-promise');
-// Just changed request to promise based library -- everything will be broken until refactored
+const rp = require('request-promise');
 
-request('https://twitter.com/pubg_help', (error, response, html) => {
-  const parsedResults = [];
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
+const options = {
+  uri: 'https://twitter.com/pubg_help',
+  transform: function (body, response, resolvedWithFullResponse) {
+    return cheerio.load(body);
+  }
+};
 
-      // Todo: Optimize to only scrape first 3 posts
-      $('.js-stream-item').each((i, element) => {
+rp(options)
+  .then($ => {
+    const parsedResults = [];
 
-        const tweet = $(element).children('.tweet').children('.content');
+    $('.js-stream-item').each((i, element) => {
+      const tweet = $(element).children('.tweet').children('.content');
 
-        const twitterHandle = '@PUBG_help';
-        const tweetText = tweet.children('.js-tweet-text-container').children('p.TweetTextSize').text();
-        const timeStamp = tweet.children('.stream-item-header').children('small.time').children('.tweet-timestamp').text();
-        const twitterLogo = tweet.children('.stream-item-header').children('a.account-group').children('img.avatar').attr('src');
+      const twitterHandle = 'PUBG_help';
+      const tweetText = tweet.children('.js-tweet-text-container').children('p.TweetTextSize').text();
+      const timeStamp = tweet.children('.stream-item-header').children('small.time').children('.tweet-timestamp').text();
+      const twitterLogo = tweet.children('.stream-item-header').children('a.account-group').children('img.avatar').attr('src');
 
-        // Todo: Clean up timestamp
-        // isNan(timeStamp.split('')[0]) ?
-        //     timeStamp = timeStamp.split('').splice(0,2).join('') :
-        //     timeStamp
-
-        const metaData = {
-          post: i + 1,
-          twitterHandle: twitterHandle,
-          twitterLogo: twitterLogo,
-          timestamp: timeStamp,
-          tweetText: tweetText
-        };
-        parsedResults.push(metaData)
-      });
-    };
+      const metaData = {
+        post: i + 1,
+        twitterHandle: twitterHandle,
+        twitterLogo: twitterLogo,
+        timestamp: timeStamp,
+        tweetText: tweetText
+      };
+      parsedResults.push(metaData)
+    })
+    console.log(parsedResults);
     return parsedResults;
-  });
+  })
+  .catch(err => {
+    console.error('Error Happened...', err);
+  })
 
 module.exports = {
   scrapeTwitter
