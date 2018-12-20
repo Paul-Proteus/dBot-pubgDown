f601c1f3e900bcb6af6e95c48485171b86c06616
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-12-20 14:57:07 -0800

    added edge case support if users trigger bot with @pubgdown, created a help message and new _help command

--- bot.js
+++ bot.js
@@ -10,23 +10,37 @@ bot.on('ready', (e) => {
 
 bot.on('message', async message => {
   
+  
+  // Edge case if users try to access bot via mentioning
+  if (message.content.startsWith('@pubgdown') || message.content.startsWith('@pubgdown!')) {
+    message.channel.send( '```My prefix is _ \n\n_check : Check if servers are down```');
+  };
+
   // Ignore bots and messages that don't start with prefix
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+  
   // Our standard argument/command name definition.
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-
+  
   const sendReport = async () => {
     ddEmbed = await buildDDEmbed();
     twitterEmbed = await buildTwitterEmbed();
     await message.channel.send(ddEmbed);
     await message.channel.send(twitterEmbed);
-  }
+  };
   
+  /**
+   * Command list 
+   */
+
   if (command === 'check') {
     await sendReport();
-    }
+  };
+  
+  if (command === 'help' || command === 'info') {
+    message.channel.send('```My prefix is _\n\nI only do one thing and I do it well...\n\n _check : Check if servers are down\n\n_help: This is the help screen (lol)``` ')
+  };
 
 });
 
